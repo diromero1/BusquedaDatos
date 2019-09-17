8f5a221c3444daedcc7da086193449a8b17b3139
diromero1 <you@example.com>
2019-09-17 18:52:32 -0500

    Paginacion

--- front/src/App.js
+++ front/src/App.js
@@ -6,12 +6,14 @@ import NavioComp from './component/navioComp'
 class App extends React.Component {
   state = {
     url: '',
-    datos: []
+    datos: [],
+    pagina: 1
   }
 
 
   searchData = (url) => {
-    fetch(url)
+    console.log(url+'$limit='+this.state.pagina*100+'&$offset='+(this.state.pagina-1)*100)
+    fetch(url+'$limit='+this.state.pagina*100+'&$offset='+(this.state.pagina-1)*100)
       .then(res => res.json())
       .then(data => {
         this.setState({
@@ -22,11 +24,48 @@ class App extends React.Component {
       .then(NavioComp.update);
   }
 
+  irAtras = () => {
+    if(this.state.pagina !== 1){
+      this.setState({pagina: this.state.pagina-1});
+      console.log("Pagina", this.state.pagina)
+    }else{
+      console.log("Estas en la pagina 1");
+    }
+  }
+
+  irAdelante = () => {
+    if(this.state.pagina*100>=this.state.datos.length){
+      this.setState({pagina: this.state.pagina+1});
+      console.log("Pagina", this.state.pagina)
+    }else{
+      console.log("Estas en la ultima pagina");
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+  // only update chart if the data has changed
+  let url = this.state.url
+  if (prevState.pagina !== this.state.pagina) {
+    console.log(url+'$limit='+this.state.pagina*100+'&$offset='+(this.state.pagina-1)*100)
+    fetch(url+'$limit='+this.state.pagina*100+'&$offset='+(this.state.pagina-1)*100)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          datos: data,
+          url: url
+        });
+      })
+      .then(NavioComp.update);
+  }
+}
+
   render(){
     return(<div>
         <h1>URL:</h1>
         <Url searchData={this.searchData}/>
         <NavioComp data={this.state.datos}/>
+        <button onClick={this.irAtras}>Atras</button>
+        <button onClick={this.irAdelante}>Adelante</button>
       </div>)
   }
 }
